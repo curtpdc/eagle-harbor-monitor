9cfa41d8d43242b0e1734cd87e85b8a301b0ad83
Curtis Prince <curtis.prince@xigusa.com>
2026-02-03 20:02:06 -0500

    Update frontend config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://eagle-harbor.centralus.azurecontainer.io:8000',
   },
